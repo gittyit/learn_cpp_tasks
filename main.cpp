8f2eef702e374343a10e9721825b393afc5eaa05
Vitalii Murashov <45685775-vitalymur@users.noreply.replit.com>
2025-07-31 12:22:45 +0000

    2.5.2

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
-#include <string.h>
 
-// 2.5.1
+// 2.5.2
 int main() {
-  std::string playerName = "Шумахер";
-  int carSpeed, lapNumber = 4;
-  int carEnginePower = 254;
-  int carWheelsPower = 93;
-  int carSteeringWheelPower = 93;
-  int weatherWindPower = 21, weatherRainPower = 17;
+  int totalAmount = 0, quantity, deliveryCost, discount;
 
-  carSpeed = carEnginePower + carWheelsPower + carSteeringWheelPower - weatherWindPower - weatherRainPower;
-
-  std::cout << "===================\n";
-  std::cout << "Супер гонки. Круг " << lapNumber << "\n";
-  std::cout << "===================\n";
-  std::cout << playerName << " (" << carSpeed << ")\n";
-  std::cout << "===================\n";
-  std::cout << "Водитель: " << playerName << "\n";
-  std::cout << "Скорость: " << carSpeed << "\n";
-  std::cout << "-------------------\n";
-  std::cout << "Оснащение\n";
-  std::cout << "Двигатель: +" << carEnginePower << "\n";
-  std::cout << "Колеса: +" << carWheelsPower << "\n";
-  std::cout << "Руль: +" << carSteeringWheelPower << "\n";
-  std::cout << "-------------------\n";
-  std::cout << "Действия плохой погоды\n";
-  std::cout << "Ветер: -" << weatherWindPower << "\n";
-  std::cout << "Дождь: -" << weatherRainPower << "\n";
+  std::cout << totalAmount;
 }
