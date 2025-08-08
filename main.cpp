76e5b38bf0dbefc46e0aa68b9e64ae39d3a9a02c
Vitalii <105340282+gittyit@users.noreply.github.com>
2025-08-08 10:16:56 +0000

    2.5.2 done

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 
 // 2.5.2
 int main() {
-  int totalAmount = 0, quantity, deliveryCost, discount;
+  int totalAmount = 0, quantity = 10, price = 50, deliveryCost = 30,
+  discount = 40, goodsCost;
 
-  std::cout << totalAmount;
+  goodsCost = quantity * price;
+  totalAmount = goodsCost + deliveryCost - discount;
+
+  std::cout << "Калькулятор для расчёта стоимости товара.\n";
+  std::cout << "Стоимость товара: " << goodsCost << "\n";
+  std::cout << "Стоимость доставки: " << deliveryCost << "\n";
+  std::cout << "Скидка: " << discount << "\n";
+  std::cout << "=========================================\n";
+  std::cout << "Полная стоимость товара: " << totalAmount;
 }
