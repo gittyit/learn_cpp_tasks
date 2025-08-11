4257cbcf0743a8bac4adeb89c2c17eed4d05974b
Vitalii <105340282+gittyit@users.noreply.github.com>
2025-08-11 10:49:08 +0000

    3.2.2 done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 
-// 2.5.2
 int main() {
-  int totalAmount = 0, quantity = 10, price = 50, deliveryCost = 30,
-  discount = 40, goodsCost;
 
-  goodsCost = quantity * price;
-  totalAmount = goodsCost + deliveryCost - discount;
+  int productCost;
+  int deliveryCost;
+  int discount;
 
-  std::cout << "Калькулятор для расчёта стоимости товара.\n";
-  std::cout << "Стоимость товара: " << goodsCost << "\n";
-  std::cout << "Стоимость доставки: " << deliveryCost << "\n";
+  int price = productCost + deliveryCost - discount;
+
+  std::cout << "Введите данные для расчета.\n";
+  std::cout << "Стоимость товара: ";
+  std::cin >> productCost;
+  std::cout << "Cтоимость доставки: ";
+  std::cin >> deliveryCost;
+  std::cout << "Скидка: ";
+  std::cin >> discount;
+
+  price = productCost + deliveryCost - discount;
+  std::cout << "\n==== РАСЧЕТ ====\n";
+  std::cout << "Стоимость товара: " << productCost << "\n";
+  std::cout << "Cтоимость доставки: " << deliveryCost << "\n";
   std::cout << "Скидка: " << discount << "\n";
-  std::cout << "=========================================\n";
-  std::cout << "Полная стоимость товара: " << totalAmount;
+  std::cout << "---------\n";
+  std::cout << "Итого: " << price << "\n";
+
 }
