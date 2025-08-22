f1bd032b47243ba40d3433656650997b8d0428f6
Vit <vitalymur@gmail.com>
2025-08-22 15:58:34 +0300

    30.4.1 done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,58 @@
 #include <iostream>
 #include <cpr/cpr.h>
-//test
-int main() {
-  int totalAmount = 0;
 
-  cpr::Response r = cpr::Get(cpr::Url("https://httpbin.org/get"));
+bool check_cmd(std::string cmd) {
+  std::string cmds[] = {"get","post", "put", "delete", "patch"};
+  for (std::string c : cmds) {
+    if (cmd == c) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void send_request(std::string cmd) {
+  cpr::Response r;
+  std::string link = "http://httpbin.org/";
+
+  link.append(cmd);
+  printf("link: %s\n", link.c_str());
+  cpr::Url url = cpr::Url(link);
+
+  if (cmd == "get") {
+    r = cpr::Get(url);
+  } else if (cmd == "post") {
+    r = cpr::Post(url, cpr::Payload{{"key", "new value"}});
+      // cpr::Header{{"Accept", "application/json"}});
+  } else if (cmd == "put") {
+    r = cpr::Put(url, cpr::Payload{{"key", "new value"}});
+  } else if (cmd == "delete") {
+    r = cpr::Delete(url, cpr::Payload{{"key", "new value"}});
+  } else if (cmd == "patch") {
+    r = cpr::Patch(url, cpr::Payload{{"key", "new value"}});
+  } else {
+    return;
+  }
+
   std::cout << "text:\n" << r.text;
   if (r.status_code == 200) {
-    std::cout << "done\n";
+    std::cout << "===DONE===\n\n";
+  } else {
+    std::cout << "===ERROR===\n\n";
   }
+}
+
+int main() {
+  
+  std::string cmd;
+
+  while (cmd != "exit")
+  {
+    std::cout << "Enter command: ";
+    std::cin >> cmd;
+    if (check_cmd(cmd)) {
+      send_request(cmd);
+    }
+  }
+  
 }
