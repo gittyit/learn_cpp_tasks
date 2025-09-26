4b44aa20713b44da4a537cef8366ce07492ac35a
vit <vitalymur@gmail.com>
2025-09-26 18:06:04 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,24 +2,31 @@
 
 int main() {
 
-  int a = 0;
-  int res;
+  int score = 0;
+  int level = 1;
   
-  std::cout << "Проверка на чётное число.\n";
+  std::cout << "Калькулятор опыта.\n";
   std::cout << "========================================================\n\n";
-  std::cout << "Введите число: ";
-  std::cin >> a;
+  std::cout << "Введите число очков опыта: ";
+  std::cin >> score;
 
-  std::cout << "\n-----Проверяем-----\n\n";
-  
-  res = a % 2;
+  std::cout << "\n-----Считаем-----\n\n";
 
-  if (res == 0) {
-    std::cout << "Число " << a << " — чётное\n";
+  if (score < 0) {
+    std::cout << "Вы ввели неверное число: " << score << "\n\n";
+    return 0;
+  }
+  else if (score < 1000) {
+    level = 1;
+  } else if (score < 2500)  {
+    level = 2;
+  } else if (score < 5000) {
+    level = 3;
   } else {
-    std::cout << "Число " << a << " — нечётное\n";
+    level = 4;
   }
 
+  std::cout << "Ваш уровень: " << level;
   std::cout << std::endl << std::endl;
 
 }
