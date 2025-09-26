6606937c694fa80e4429ea3cf9ce61dc782776e5
vit <vitalymur@gmail.com>
2025-09-26 17:18:18 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,25 +2,29 @@
 
 int main() {
 
-  int a = 42;
-  int b = 153;
+  int a = 0;
+  int b = 0;
+  int sum = 0;
+  int res = 0;
   std::string result;
   
-  std::cout << "Минимум из двух чисел.\n";
+  std::cout << "Складываем в уме.\n";
   std::cout << "========================================================\n\n";
   std::cout << "Введите первое число: ";
   std::cin >> a;
   std::cout << "Введите второе число: ";
   std::cin >> b;
+  std::cout << "Введите их сумму: ";
+  std::cin >> sum;
 
   std::cout << "\n-----Проверяем-----\n\n";
+  
+  res = a + b;
 
-  if (a == b) {
-    std::cout << "Числа равны!\n";
-  } else if (a > b) {
-    std::cout << "Наименьшее число: " << b;
-  } else if (a < b) {
-    std::cout << "Наименьшее число: " << a;
+  if (sum == res) {
+    std::cout << "Верно!\n";
+  } else {
+    std::cout << "Ошибка! Верный результат: " << res;
   }
 
   std::cout << std::endl << std::endl;
