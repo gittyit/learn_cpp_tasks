2ad7f7d1dbf0c16781d0f8d26ba94034679d4262
vit <vitalymur@gmail.com>
2025-10-14 15:21:15 +0300

    done

--- main.cpp
+++ main.cpp
@@ -2,51 +2,107 @@
 
 int main() {
 
-  int amount, sum = 0;
-  int m1 = 100, m2 = 200, m3 = 500, m4 = 1000,
-      m5 = 2000, m6 = 5000;
-  int mq1 = 10, mq2 = 5, mq3 = 2, mq4 = 5,
-      mq5 = 2, mq6 = 2;
+  int a, b, c;
+  int m, n, k;
+  bool ok = false;
 
-  std::cout << "Банкомат — 2.\n\n";
-  std::cout << "Введите сумму: ";
-  std::cin >> amount;
+  std::cout << "Усложнение задачи про кирпич.\n\n";
+  std::cout << "Введите размер первой коробки (A×B×C): ";
+  std::cin >> a >> b >> c;
+  std::cout << "Введите размер второй коробки (M×N×K): ";
+  std::cin >> m >> n >> k;
 
-  if (amount <= 0 || amount > 150000 || amount % 100 ||
-      amount > (m6*mq6 + m5*mq5 + m4*mq4 + m3*mq3 + m2*mq2 + m1*mq1)) {
-    if (amount <= 0) std::cout << "Неверная сумма: " << amount;
-    if (amount > 150000)
-      std::cout << "\nСумма превышает лимит выдачи за один раз.";
-    if (amount % 100)
-      std::cout << "\nВыдать ровно " << amount << " рублей невозможно.";
-    if (amount > (m6*mq6 + m5*mq5 + m4*mq4 + m3*mq3 + m2*mq2 + m1*mq1))
-      std::cout << "\nВ банкомате не достаточно наличных.";
-    std::cout << std::endl << std::endl;
-    return 0;
+  
+  // X-Y-Z:
+  // ------
+  // 1. CxB-A - NxK-M
+  // 2. CxB-A - MxK-N
+  // 3. CxB-A - MxN-K
+  // 4. AxC-B - NxK-M
+  // 5. AxC-B - MxK-N
+  // 6. AxC-B - MxN-K
+  // 7. AxB-C - NxK-M
+  // 8. AxB-C - MxK-N
+  // 9. AxB-C - MxN-K
+
+  std::cout << "\nВарианты:\n";
+
+  // 1. CxB-A - NxK-M
+  if (c <= n && b <= k && a <= m ) {
+    std::cout << "\n1.   C×B×A = " << c << "x" << b << "x" << a;
+    std::cout << "\n     N×K×M = " << n << "x" << k << "x" << m << std::endl;
+    ok = true;
   }
+  // 2. CxB-A - MxK-N
+  if (c <= m && b <= k && a <= n ) {
+    std::cout << "\n2.   C×B×A = " << c << "x" << b << "x" << a;
+    std::cout << "\n     M×K×N = " << m << "x" << k << "x" << n << std::endl;
+    ok = true;
+  }
+  // 3. CxB-A - MxN-K
+  if (c <= m && b <= n && a <= k ) {
+    std::cout << "\n3.   C×B×A = " << c << "x" << b << "x" << a;
+    std::cout << "\n     M×N×K = " << m << "x" << n << "x" << k << std::endl;
+    ok = true;
+  }
+  // 4. AxC-B - NxK-M
+  if (a <= n && c <= k && b <= m ) {
+    std::cout << "\n4.   A×C×B = " << a << "x" << c << "x" << b;
+    std::cout << "\n     N×K×M = " << n << "x" << k << "x" << m << std::endl;
+    ok = true;
+  }
+  // 5. AxC-B - MxK-N
+  if (a <= m && c <= k && b <= n ) {
+    std::cout << "\n5.   A×C×B = " << a << "x" << c << "x" << b;
+    std::cout << "\n     M×K×N = " << m << "x" << k << "x" << n << std::endl;
+    ok = true;
+  }
+  // 6. AxC-B - MxN-K
+  if (a <= m && c <= n && b <= k ) {
+    std::cout << "\n6.   A×C×B = " << a << "x" << c << "x" << b;
+    std::cout << "\n     M×N×K = " << m << "x" << n << "x" << k << std::endl;
+    ok = true;
+  }
+  // 7. AxB-C - NxK-M
+  if (a <= n && b <= k && c <= m ) {
+    std::cout << "\n7.   A×B×C = " << a << "x" << b << "x" << c;
+    std::cout << "\n     N×K×M = " << n << "x" << k << "x" << m << std::endl;
+    ok = true;
+  }
+  // 8. AxB-C - MxK-N
+  if (a <= m && b <= k && c <= n ) {
+    std::cout << "\n8.   A×B×C = " << a << "x" << b << "x" << c;
+    std::cout << "\n     M×K×N = " << m << "x" << k << "x" << n << std::endl;
+    ok = true;
+  }
+  // 9. AxB-C - MxN-K
+  if (a <= m && b <= n && c <= k ) {
+    std::cout << "\n9.   A×B×C = " << a << "x" << b << "x" << c;
+    std::cout << "\n     M×N×K = " << m << "x" << n << "x" << k << std::endl;
+    ok = true;
+  }
+  
+
+  if (ok) std::cout << "\n\nПервая коробка помещается во вторую.";
   else {
-    std::cout << "\nВыдача наличных:\n";
-    for(;mq6 > 0 && (amount - m6 >= 0);mq6--) {
-      std::cout << m6 << " рублей.\n"; amount -= m6; sum += m6;
-    }
-    for(;mq5 > 0 && (amount - m5 >= 0);mq5--) {
-      std::cout << m5 << " рублей.\n"; amount -= m5; sum += m5;
-    }
-    for(;mq4 > 0 && (amount - m4 >= 0);mq4--) {
-      std::cout << m4 << " рублей.\n"; amount -= m4; sum += m4;
-    }
-    for(;mq3 > 0 && (amount - m3 >= 0);mq3--) {
-      std::cout << m3 << " рублей.\n"; amount -= m3; sum += m3;
-    }
-    for(;mq2 > 0 && (amount - m2 >= 0);mq2--) {
-      std::cout << m2 << " рублей.\n"; amount -= m2; sum += m2;
-    }
-    for(;mq1 > 0 && (amount - m1 >= 0);mq1--) {
-      std::cout << m1 << " рублей.\n"; amount -= m1; sum += m1;
-    }
-    std::cout << "\nИтого: " << sum << " рублей.\n";
-    std::cout << "\nГотово.\n";
+    std::cout << "нет\n";
+    std::cout << "\nПервая коробка не помещается во вторую.";
   }
+
+
+  // Старая задача.
+  
+  // int x, y;
+  // 
+  // if (a <= x && b <= y || a <= y && b <= x) {
+  //   std::cout << "Можно";
+  // } else if (b <= x && c <= y || b <= y && c <= x) {
+  //   std::cout << "Можно";
+  // } else if (a <= x && c <= y || a <= y && c <= x) {
+  //   std::cout << "Можно";
+  // } else {
+  //   std::cout << "Нельзя";
+  // }
 
   std::cout << std::endl << std::endl;
 
